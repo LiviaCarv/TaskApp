68b9be098712c7a5e45a0944652d777121d77309
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-04-10 19:58:59 -0400

    implemented LoginFragment layout

---
+++ app/src/main/java/com/project/taskapp/ui/auth/LoginFragment.kt
@@ -0,0 +1,31 @@
+package com.project.taskapp.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.project.taskapp.R
+import com.project.taskapp.databinding.FragmentLoginBinding
+import com.project.taskapp.databinding.FragmentSplashBinding
+
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
