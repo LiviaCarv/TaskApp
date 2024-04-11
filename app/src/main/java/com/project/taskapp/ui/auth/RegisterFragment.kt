336381ac61ba504d916c95bf507941f05afb6fa5
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-04-10 20:10:57 -0400

    implemented RegisterFragment layout

---
+++ app/src/main/java/com/project/taskapp/ui/auth/RegisterFragment.kt
@@ -0,0 +1,30 @@
+package com.project.taskapp.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.project.taskapp.R
+import com.project.taskapp.databinding.FragmentLoginBinding
+import com.project.taskapp.databinding.FragmentRegisterBinding
+
+
+class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
