838cde7cef11819c751bfed9bdcd8d502bccf7b8
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-04-11 17:55:30 -0400

    completed navigation to registerFragment and recoverAccountFragment

--- app/src/main/java/com/project/taskapp/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/project/taskapp/ui/auth/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.project.taskapp.R
 import com.project.taskapp.databinding.FragmentLoginBinding
 import com.project.taskapp.databinding.FragmentSplashBinding
@@ -21,6 +22,21 @@ class LoginFragment : Fragment() {
     ): View? {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        binding.btnRecoverPassword.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_recoverAccountFragment)
+        }
     }
 
     override fun onDestroyView() {
