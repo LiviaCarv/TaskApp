15af6698328716d448169a9167ca7571cc0b2474
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-04-10 21:21:45 -0400

    initializing app navigation

--- app/src/main/java/com/project/taskapp/ui/SplashFragment.kt
+++ app/src/main/java/com/project/taskapp/ui/SplashFragment.kt
@@ -1,10 +1,13 @@
 package com.project.taskapp.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.project.taskapp.R
 import com.project.taskapp.databinding.FragmentSplashBinding
 
@@ -19,6 +22,15 @@ class SplashFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentSplashBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed(this::checkAuthentication, 3000)
+    }
+
+    private fun checkAuthentication() {
+        findNavController().navigate(R.id.action_splashFragment_to_authentication)
     }
 
     override fun onDestroyView() {
