6dc2792b7fff768d33535f011fd819a7bfd815c4
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-04-15 20:33:10 -0400

    Implemented user logout functionality

--- app/src/main/java/com/project/taskapp/ui/HomeFragment.kt
+++ app/src/main/java/com/project/taskapp/ui/HomeFragment.kt
@@ -5,11 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.project.taskapp.R
 import com.project.taskapp.databinding.FragmentHomeBinding
 import com.project.taskapp.ui.adapter.ViewPagerAdapter
+import com.project.taskapp.util.showBottomSheet
 
 
 class HomeFragment : Fragment() {
@@ -17,6 +22,7 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var viewPager: ViewPager2
     private lateinit var viewPagerAdapter: ViewPagerAdapter
+    private lateinit var auth: FirebaseAuth
 
 
     override fun onCreateView(
@@ -29,8 +35,23 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        auth = Firebase.auth
         initTabLayout()
+        logoutListener()
+    }
 
+    private fun logoutListener() {
+        binding.btnLogout.setOnClickListener {
+            showBottomSheet(
+                message = getString(R.string.text_message_logout),
+                titleDialog = R.string.title_dialog_confirm_logout,
+                titleButton = R.string.title_button_logout,
+                onClick = {
+                    auth.signOut()
+                    findNavController().navigate(R.id.action_homeFragment_to_authentication)
+                }
+            )
+        }
     }
 
     private fun initTabLayout() {
